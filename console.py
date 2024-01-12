c3eb42fc54928cfa9a485432b9c6678b3ac1b7d4
Badr_Annabi <badrannabi@gmail.com>
2024-01-12 13:56:41 +0100

    all tasks

--- console.py
+++ console.py
@@ -7,7 +7,23 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 
-valdated_classes = {'BaseModel': BaseModel}
+validated_classes = {'BaseModel': BaseModel}
+
+def validated_args(args, check_id=False):
+        """Checks on args to validated classname entry"""
+        if len(args) < 1:
+            print("** class name missing **")
+            return False
+        class_name = args[0]
+        if class_name not in validated_classes and class_name not in globals():
+            print("** class doesn't exist **")
+            return False
+        if len(args) < 2 and check_id:
+            print("** instance id missing **")
+            return False
+        return True
+
+
 class HBNBCommand(cmd.Cmd):
     """
         contains: EOF, quit commands to exit the program
@@ -15,23 +31,92 @@ class HBNBCommand(cmd.Cmd):
 
     """
     prompt = "(hbnb) "
-
+  
+    
+    
     def do_create(self, arg):
         """Create command creates a new instance of BaseModel"""
         if not arg:
             print("** class name missing **")
             return 
-        class_name = arg
+        args = arg.split()
+        if not validated_args(args):
+            return
+        new_instance = validated_classes[args[0]]()
+        new_instance.save()
+        print(new_instance.id)
+    
+    def do_show(self, arg):
+        """Show command Prints the string representation
+of an instance based on the class name and id."""
+        args = arg.split()
+        
+        if not validated_args(args, check_id=True):
+            return
+        obj = storage.all()
+        key = f"{args[0]}.{args[1]}"
+        req_instance = obj.get(key, None)
+        if req_instance is None:
+            print("** no instance found **")
+            return
+        print(req_instance)
+    
+    def do_all(self, arg):
+        """all command Prints all string representation
+of all instances based or not on the class name."""
 
-        try:
-            class_obj = valdated_classes[class_name]
-            new_instance = class_obj()
-            new_instance.save()
-            print(new_instance.id)
+        args = arg.split()
+        objects = storage.all()
+        if len(args) < 1:
+            print(["{}".format(str(obj)) for key, obj in objects.items()])
+            return
+        if args[0] not in validated_classes:
+            print("** class doesn't exist **")        
+        else:
+            print(["{}".format(str(obj)) for key, obj in objects.items()])
+            return
 
-        except KeyError:
-            print("** class doesn't exist **")
-            
+    def do_destroy(self, arg):
+        """Destroy command Deletes an instance based
+on the class name and id (save the change into the JSON file)."""
+        args = arg.split()
+
+        if not validated_args(args, check_id=True):
+            return
+        obj = storage.all()
+        key = f"{args[0]}.{args[1]}"
+        req_instance = obj.get(key, None)
+        if req_instance is None:
+            print("** no instance found **")
+            return
+        obj.pop(key)
+        storage.save()
+    
+    def do_update(self, arg):
+        """Reload command Updates an instance based on
+the class name and id by adding or updating attribute
+(save the change into the JSON file)."""
+        args = arg.split()
+        obj = storage.all()
+        if not validated_args(args, check_id=True):
+            return
+        key = f"{args[0]}.{args[1]}"
+        req_instance = obj.get(key, None)
+        if req_instance is None:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+        if len(args) < 4:
+            print("** value missing **")
+        else:    
+            attr_name = args[2]
+            attr_value = args[3]
+            setattr(obj[key], attr_name, eval(attr_value))
+            storage.save()
+        
+        
+
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
