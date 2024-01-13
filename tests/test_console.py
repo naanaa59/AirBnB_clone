af4f4c817455662e50c316f70a8ee7fd4bd43f9d
oumaima <oumaimanaanaapro@gmail.com>
2024-01-13 19:39:49 +0100

    test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    Test file for Console class
+"""
+from console import HBNBCommand
+import unittest
+
+
+class Test_HBNBCommand(unittest.TestCase):
+    """
+        test class for the cosnsole HBNB
+    """
